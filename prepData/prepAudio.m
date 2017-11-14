9aaa62b20d8e3708400e779b1f0a0c2665fe29d2
ttanprasert <ttanprasert@hmc.edu>
2017-11-13 17:53:51 -0800

    remove songlist

--- prepData/prepAudio.m
+++ prepData/prepAudio.m
@@ -1,4 +1,4 @@
-function prepAudio (artist,fileList,songList, outdir)
+function prepAudio (artist,fileList,outdir)
 
 % prepAudio convert all songs in the fileList to be in mono channel
 % and has fs = 22050 Hz. It also renames the song to be in the format
@@ -8,8 +8,6 @@ function prepAudio (artist,fileList,songList, outdir)
 % Input:
 % - artist: a string contains name of the artist
 % - fileList: a .list file that contains path to all files we want to prep.
-% - songList: a cell array of song name strings. The order of the song name
-%             must corresponds to the order of file paths in fileList.
 % - outdir: the directory where you want the prepped audio to be saved at.
 
 fid = fopen(fileList);
@@ -21,7 +19,7 @@ while ischar(curfile)
     [y,fs] = audioread(curfile);
     y = sum(y,2)./2; % mono
     y = resample(y,22050,fs);
-    newFileName = strcat(outdir,'/',artist,'_',char(songList(songcount)),'_',int2str(i),'.wav');
+    newFileName = strcat(outdir,'/',artist,'_Ref',int2str(songcount),'_',int2str(i),'.wav');
     audiowrite(newFileName,y,22050); 
     i = i+1;
     if i == 3
