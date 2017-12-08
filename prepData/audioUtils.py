054a914c40a2d3ab3d5a822de0c273751b63af28
Teerapat Jenrungrot <mjenrungrot@hmc.edu>
2017-12-08 01:35:29 -0800

    Added working full DTW

--- prepData/audioUtils.py
+++ prepData/audioUtils.py
@@ -3,6 +3,7 @@ import sys
 import numpy as np
 import librosa
 import csv
+import scipy.signal
 
 def trim(sig, fs, startTime=None, endTime=None, verbose=False):
     """
@@ -88,6 +89,7 @@ def generateStereoRefTrack(concertTrack, albumTrack, fs, segmentLength=2, silenc
         rightChannel = np.concatenate((rightChannel, albumSegment, remaining))
 
         currentIndex = segmentEndIndex
+        if(verbose): print("left = {:} right = {:}".format(leftChannel.shape, rightChannel.shape))
 
     assert(leftChannel.shape[0] == rightChannel.shape[0])
 
@@ -121,7 +123,7 @@ def getMFCCdelta2(sig, fs, n_mfcc=20):
     mfcc_delta_delta = librosa.feature.delta(mfcc, order=2)
     return mfcc_delta_delta
 
-def getDTWpath(concertTrack, albumTrack, fs, chunkLength=6.0, hop_size=512, metric='cosine', verbose=False, feature="mfcc"):
+def getDTWpath(concertTrack, albumTrack, fs, chunkLength=6.0, hop_size=512, metric='cosine', verbose=False, feature="mfcc", full_DTW=False):
     """
     Get subsequence DTW path.
 
@@ -157,37 +159,66 @@ def getDTWpath(concertTrack, albumTrack, fs, chunkLength=6.0, hop_size=512, metr
     else:
         raise ValueError("feature can only be 'chroma' or 'mfcc'")
 
-    prefix = 0
-    DTW_path = []
-    offsetChunks = []
+    if not full_DTW:
+        prefix = 0
+        DTW_path = []
+        offsetChunks = []
 
-    albumTrackFeature = X_2
-    concertTrackFeatures = []
-    for i in range(len(concertChunks)):
+        albumTrackFeature = X_2
+        concertTrackFeatures = []
+        for i in range(len(concertChunks)):
+            if feature == "chroma":
+                X_1 = getChroma(concertChunks[i], fs)
+            elif feature == "mfcc":
+                X_1 = getMFCCdelta2(concertChunks[i], fs)
+
+            concertTrackFeatures.append(X_1)
+
+            X_1 += np.finfo(np.float32).eps
+            X_2 += np.finfo(np.float32).eps
+
+            # Y = long sequence
+            # X = relatively short sequence
+            D, wp = librosa.core.dtw(X=X_1, Y=X_2, metric=metric, \
+                                    step_sizes_sigma=np.array([[1,1],[1,2],[2,1]]),\
+                                    weights_add=np.array([1,1,2]),\
+                                    weights_mul=np.array([1,1,1]),\
+                                    subseq=True)
+
+            for (concertChunkPath, albumTrackPath) in wp[::-1,:]:
+                newPoint = (concertChunkPath + prefix, albumTrackPath)
+                DTW_path.append(newPoint)
+
+            offsetChunks.append(prefix)
+            if(verbose): print("Chunk {:} offset = {:}[{:}]".format(i, wp[-1][1] * hop_size / fs, prefix))
+
+            prefix += X_1.shape[1]
+        return DTW_path, offsetChunks, concertTrackFeatures, albumTrackFeature
+
+    else: 
         if feature == "chroma":
-            X_1 = getChroma(concertChunks[i], fs)
+            X_1 = getChroma(concertTrack, fs)
         elif feature == "mfcc":
-            X_1 = getMFCCdelta2(concertChunks[i], fs)
+            X_1 = getMFCCdelta2(concertTrack, fs)
 
-        concertTrackFeatures.append(X_1)
+        DTW_path = []
+
+        albumTrackFeature = X_2
+        concertTrackFeature = X_1
 
         # Y = long sequence
         # X = relatively short sequence
         D, wp = librosa.core.dtw(X=X_1, Y=X_2, metric=metric, \
-                                 step_sizes_sigma=np.array([[1,1],[1,2],[2,1]]),\
-                                 weights_add=np.array([1,1,2]),\
-                                 weights_mul=np.array([1,1,1]),\
-                                 subseq=True)
+                                step_sizes_sigma=np.array([[1,1],[1,2],[2,1]]),\
+                                weights_add=np.array([1,1,2]),\
+                                weights_mul=np.array([1,1,1]),\
+                                subseq=False)
 
         for (concertChunkPath, albumTrackPath) in wp[::-1,:]:
-            newPoint = (concertChunkPath + prefix, albumTrackPath)
+            newPoint = (concertChunkPath, albumTrackPath)
             DTW_path.append(newPoint)
 
-        offsetChunks.append(prefix)
-        if(verbose): print("Chunk {:} offset = {:}[{:}]".format(i, wp[-1][1] * hop_size / fs, prefix))
-
-        prefix += X_1.shape[1]
-    return DTW_path, offsetChunks, concertTrackFeatures, albumTrackFeature
+        return DTW_path, [0], concertTrackFeature, albumTrackFeature
 
 def outputDTWPathCSV(DTW_path, outputFile):
     with open(outputFile, 'w') as csvfile:
@@ -198,30 +229,64 @@ def outputDTWPathCSV(DTW_path, outputFile):
 
 
 import bisect
-def getAlignedAlbumTrack(DTW_path, offsets, albumTrack, fs, hop_size=512, chunkLength=6.0, verbose=False):
-    output = np.array([])
-    framePerSeconds = fs//hop_size
-    for idx in range(len(offsets)):
-        currentTime = chunkLength * idx
-        mark1 = (int(framePerSeconds * currentTime + idx), np.inf)
-        mark2 = (int(framePerSeconds * (currentTime + chunkLength / 3) + idx), np.inf)
-        mark3 = (int(framePerSeconds * (currentTime + chunkLength * 2 / 3) + idx), np.inf)
+def getAlignedAlbumTrack(DTW_path, offsets, albumTrack, fs, hop_size=512, chunkLength=6.0, verbose=False, full_DTW=False):
+    if not full_DTW:
+        output = np.array([])
+        framePerSeconds = fs//hop_size
+        for idx in range(len(offsets)):
+            currentTime = chunkLength * idx
+            mark1 = (int(framePerSeconds * currentTime + idx), np.inf)
+            mark2 = (int(framePerSeconds * (currentTime + chunkLength / 3) + idx), np.inf)
+            mark3 = (int(framePerSeconds * (currentTime + chunkLength * 2 / 3) + idx), np.inf)
 
-        time1 = DTW_path[bisect.bisect_left(DTW_path, mark1)-1][1] * hop_size / fs
-        time2 = DTW_path[bisect.bisect_left(DTW_path, mark2)-1][1] * hop_size / fs
-        time3 = DTW_path[bisect.bisect_left(DTW_path, mark3)-1][1] * hop_size / fs
+            time1 = DTW_path[bisect.bisect_left(DTW_path, mark1)-1][1] * hop_size / fs
+            time2 = DTW_path[bisect.bisect_left(DTW_path, mark2)-1][1] * hop_size / fs
+            time3 = DTW_path[bisect.bisect_left(DTW_path, mark3)-1][1] * hop_size / fs
 
-        index1 = int(time1 * fs)
-        index2 = int(time2 * fs)
-        index3 = int(time3 * fs)
+            index1 = int(time1 * fs)
+            index2 = int(time2 * fs)
+            index3 = int(time3 * fs)
 
-        target1 = index1 + int(fs * chunkLength / 3)
-        target2 = index2 + int(fs * chunkLength / 3)
-        target3 = index3 + int(fs * chunkLength / 3)
-        if(verbose): print("index = {:} target = {:} {:}".format(index1, target1, (target1-index1)/fs))
-        if(verbose): print("index = {:} target = {:} {:}".format(index2, target2, (target2-index2)/fs))
-        if(verbose): print("index = {:} target = {:} {:}".format(index3, target3, (target3-index3)/fs))
-        output = np.concatenate((output, albumTrack[index1:target1]))
-        output = np.concatenate((output, albumTrack[index2:target2]))
-        output = np.concatenate((output, albumTrack[index3:target3]))
-    return output, fs
+            target1 = index1 + int(fs * chunkLength / 3)
+            target2 = index2 + int(fs * chunkLength / 3)
+            target3 = index3 + int(fs * chunkLength / 3)
+            if(verbose): print("index = {:} target = {:} {:}".format(index1, target1, (target1-index1)/fs))
+            if(verbose): print("index = {:} target = {:} {:}".format(index2, target2, (target2-index2)/fs))
+            if(verbose): print("index = {:} target = {:} {:}".format(index3, target3, (target3-index3)/fs))
+            output = np.concatenate((output, albumTrack[index1:target1]))
+            output = np.concatenate((output, albumTrack[index2:target2]))
+            output = np.concatenate((output, albumTrack[index3:target3]))
+        return output, fs
+    else:
+        output = np.array([])
+        framePerSeconds = fs // hop_size
+        
+        # DTW_path is a list of pair of DTW path in the form of (x,y)
+        # where x = coordinate for concertTrack
+        #       y = coordinate for albumTrack
+
+        currentChunkNo = 0
+        currentFrame = 0
+        while True:
+            # Find the ending of the 6-sec queryChunk in the DTW path array 
+            marker = (int((currentChunkNo + 1) * chunkLength * fs / hop_size), np.inf)
+
+            # Obtain the frame index for the albumTrack
+            targetFrame = DTW_path[bisect.bisect_left(DTW_path, marker)-1][1]
+
+            start = int(currentFrame * hop_size)
+            end   = int(targetFrame * hop_size)
+            if(start > end): break 
+
+
+            resampled = scipy.signal.resample(albumTrack[start:end], int(chunkLength * fs)) 
+            if(verbose): print("start = {:} end = {:} length = {:} newLength = {:}".format(start, end, end-start, len(resampled)))
+
+            output = np.concatenate((output, resampled))
+
+            currentFrame = targetFrame + 1
+            currentChunkNo += 1
+
+        return output, fs
+
+
